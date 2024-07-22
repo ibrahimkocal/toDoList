353b1b0893ae15d93bf3365c25fd38910c4cded5
ibrahimkocal <ibrahiimkocal@gmail.com>
2024-07-22 17:23:01 +0300

    boş karakter eklenmemesi sağlandı

--- js/app.js
+++ js/app.js
@@ -1,7 +1,24 @@
 const taskInput = document.querySelector("#task");
 const list = document.querySelector("#list");
+const liveToastBtn = document.querySelectorAll("#liveToastBtn");
 
-function newElement(){
-    list.innerHTML += `<li>${taskInput.value}</li>` ;
-    
+// Ekle butonuna ait newElement() onclick ile tuşa tıkladıktan sonra listeye ekleme
+function newElement() {
+    // Trim metodu ile input değerinin başındaki ve sonundaki boşlukları temizleyin
+    let taskValue = taskInput.value.trim();
+
+    if (taskValue !== "") {
+        list.innerHTML += `<li>${taskValue}</li>`;
+        taskInput.value = ""; 
+    } else {
+        alert("Boş eklenemez");
+    }
 }
+
+// Input içerisinde yazma işlemini yaptıktan sonra enter tuşuna basarak direkt listeye ekleme
+taskInput.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      newElement();
+    }
+}) 
