d7438f48ced1193f1a91c2e713909afac6ada290
ibrahimkocal <ibrahiimkocal@gmail.com>
2024-07-22 20:48:04 +0300

    listede seçilen veya seçilmeyen elemanlar olarak renk ayrımı verildi.

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ const list = document.querySelector("#list");
 const liveToastBtn = document.querySelector("#liveToastBtn");
 const liveToastSuccess = document.querySelector("#liveToastSuccess");
 const liveToastError = document.querySelector("#liveToastError");
+const logo = document.querySelector("#svg");
 
 // Ekle butonuna ait newElement() onclick ile tuşa tıkladıktan sonra listeye ekleme
 function newElement() {
@@ -10,7 +11,7 @@ function newElement() {
     let taskValue = taskInput.value.trim();
 
     if (taskValue !== "") {
-        list.innerHTML += `<li>${taskValue}</li>`;
+        list.innerHTML += `<li class="alert alert-dismissible fade show"><button type="button" class="close" data-dismiss="alert">&times;</button>${taskValue}</li>`;
         taskInput.value = "";
         showToast(liveToastSuccess);
     } else {
@@ -25,6 +26,26 @@ taskInput.addEventListener("keypress", function(event) {
       newElement();
     }
 }) 
+
+
+// Liste elemanlarına tıklayıp belirtmek için sınıf ekleme-çıkarma mevzusu vs.
+list.addEventListener('click', (event) => {
+    const item = event.target.closest('li');
+    if (item) {
+        if (item.classList.contains("active")) {
+            item.classList.remove("active");
+            
+            item.style.backgroundColor = "";
+            item.style.color = "";
+        } 
+        else {
+            item.classList.add("active");
+            logo.classList.add("d-inline-block");
+            item.style.backgroundColor = "#30302f";
+            item.style.color = "white";
+        }
+    }
+});
 
 // Show to toast
 function showToast(toastElement) {
